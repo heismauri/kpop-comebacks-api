15b36a550c96196a24425cf42156c9664d156cf9
Mauricio Villa <vbamauricio@gmail.com>
2023-12-01 07:15:56 -0300

    Temporary fix for unformatted calendars

--- routes/api.js
+++ routes/api.js
@@ -47,33 +47,37 @@ const headerRegExp = () => {
 };
 
 const getComebacks = async (date) => {
-  const formattedDate = dateForURL(date);
-  const json = await fetch(`https://www.reddit.com/r/kpop/wiki/upcoming-releases/${formattedDate}.json`, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
-    }
-  }).then((response) => response.json());
-  const allComebacks = json.data.content_md
-    .split(headerRegExp())[3]
-    .split(/(\r\n\r\n|\n\n)\[Auto-updating Spotify Playlist \(Recent Title Tracks\)\]/)[0]
-    .split(/\r\n|\n/);
-  const unformattedComebacks = allComebacks.map((comeback) => {
-    // eslint-disable-next-line prefer-const
-    let [, day, time, artist, detail] = comeback.split('|');
-    if (time === '?') return false;
-    artist = artist.replace(/^\*|\*$/g, '');
-    detail = detail.replace(/^\*|\*$/g, '');
-    if (day !== '') day = /\d+/.exec(day);
-    const [year, month] = formattedDate.split('/');
-    let title = (detail === '') ? artist : `${artist} - ${detail}`;
-    title = decode(title);
-    return {
-      year, month, day, time, title
-    };
-  }).filter((comeback) => comeback);
-  return formatComebacks(unformattedComebacks)
-    .sort((a, b) => a.title.localeCompare(b.title))
-    .sort((a, b) => a.date - b.date);
+  try {
+    const formattedDate = dateForURL(date);
+    const json = await fetch(`https://www.reddit.com/r/kpop/wiki/upcoming-releases/${formattedDate}.json`, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+      }
+    }).then((response) => response.json());
+    const allComebacks = json.data.content_md
+      .split(headerRegExp())[3]
+      .split(/(\r\n\r\n|\n\n)\[Auto-updating Spotify Playlist \(Recent Title Tracks\)\]/)[0]
+      .split(/\r\n|\n/);
+    const unformattedComebacks = allComebacks.map((comeback) => {
+      // eslint-disable-next-line prefer-const
+      let [, day, time, artist, detail] = comeback.split('|');
+      if (time === '?') return false;
+      artist = artist.replace(/^\*|\*$/g, '');
+      detail = detail.replace(/^\*|\*$/g, '');
+      if (day !== '') day = /\d+/.exec(day);
+      const [year, month] = formattedDate.split('/');
+      let title = (detail === '') ? artist : `${artist} - ${detail}`;
+      title = decode(title);
+      return {
+        year, month, day, time, title
+      };
+    }).filter((comeback) => comeback);
+    return formatComebacks(unformattedComebacks)
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .sort((a, b) => a.date - b.date);
+  } catch (_) {
+    return [];
+  }
 };
 
 const getAllComebacksUpstream = async (env) => {
